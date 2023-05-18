97871b2a8926fcaf228191a35c48c0f5db04044d
bushi88 <djamcc2@hotmail.fr>
2023-05-18 11:15:52 +0200

    màj product_details

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductRepository $repoProduct): Response
     {
+        $products = $repoProduct->findAll();
+        $productBestSeller = $repoProduct->findByIsBestSeller(1);
+        $productSpecialOffer = $repoProduct->findByIsSpecialOffer(1);
+        $productNewArrival = $repoProduct->findByIsNewArrival(1);
+        $productFeatured = $repoProduct->findByIsFeatured(1);
+
+        // test
+        // dd($products);
+        // dd([$productBestSeller, $productSpecialOffer, $productNewArrival, $productFeatured]);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'products' => $products,
+            'productBestSeller' => $productBestSeller,
+            'productSpecialOffer' => $productSpecialOffer,
+            'productNewArrival' => $productNewArrival,
+            'productFeatured' => $productFeatured,
+        ]);
+    }
+
+    #[Route('/product/{slug}', name: 'app_product_details')]
+    public function show(Product $product): Response
+    {
+        if (!$product) {
+            return $this->redirectToRoute("app_home");
+        }
+
+        return $this->render('home/single_product.html.twig', [
+            'product' => $product
         ]);
     }
 }
